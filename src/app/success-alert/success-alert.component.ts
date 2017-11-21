ea8ec37e86c6a599fed335211d6199c267699844
Gea Cassinello <geacassi@publicisgroupe.net>
2017-11-21 14:42:08 -0500

    Upload Project

---
+++ src/app/success-alert/success-alert.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-success-alert',
+  templateUrl: './success-alert.component.html',
+  styleUrls: ['../app.component.css','./success-alert.component.css']
+})
+export class SuccessAlertComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
